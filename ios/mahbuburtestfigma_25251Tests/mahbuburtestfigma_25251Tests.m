7132292e397fe283e0885523039d69503b3530a1
Crowdbotics <team@crowdbotics.com>
2021-03-24 19:52:17 +0000

    Initial commit.

---
+++ ios/mahbuburtestfigma_25251Tests/mahbuburtestfigma_25251Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface mahbuburtestfigma_25251 : XCTestCase
+
+@end
+
+@implementation mahbuburtestfigma_25251
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
